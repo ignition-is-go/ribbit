823715f50dd6286e6287c30747ed7d0673ed17a3
Trevor Sargent <trevor.a.sargent@gmail.com>
2020-01-10 11:18:43 -0800

    feat: move exchange type, binding, and queue name to .env

--- index.js
+++ index.js
@@ -4,26 +4,28 @@ require('dotenv').config()
 
 const {
   CONNECTION_STRING: connectionString,
-  EXCHANGE_NAME: exchangeName, 
-  QUEUE_NAME: queue
+  EXCHANGE_NAME: exchange, 
+  EXCHANGE_TYPE: type,
+  QUEUE_NAME: queue, 
+  QUEUE_BINDING: binding
+
 } = process.env
 
 amqp.connect(connectionString, (error, connection) => {
   if(error){
     console.error(error)
   }
-
-  const queue = 'ribbit'
   connection.createChannel((error, channel) => {
     channel.assertQueue(queue, {
       durable: false
     })
 
-    channel.assertExchange(exchangeName, 'topic')
+    channel.assertExchange(exchange, type)
 
-    channel.bindQueue(queue, exchangeName, "#")
+    channel.bindQueue(queue, exchange, binding)
 
     channel.consume(queue, (msg) => {
+      channel.ack(msg)
       console.log(msg)
     })
   })
